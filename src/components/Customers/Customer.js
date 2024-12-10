dc59505386d145fa1a6b03495279bcb622deda1a
Sondos Amr <sondos.amr444@gmail.com>
2024-12-10 21:45:08 +0200

    first commit

--- src/components/Customers/Customer.js
+++ src/components/Customers/Customer.js
@@ -1,34 +1,43 @@
 export default function Customer() {
   return (
-    <div className="container-customer">
-      {data.map((customer, index) => (
-        <ContentCustomer
-          key={index}
-          img={customer.img}
-          descrpition={customer.descrpition}
-          name={customer.name}
-          position={customer.position}
-        />
-      ))}
+    <div className="wrapper-container swiper">
+      <div className="  wrapper ">
+        <ul className="container-customer swiper-wrapper">
+          {data.map((customer, index) => (
+            <ContentCustomer
+              key={index}
+              img={customer.img}
+              descrpition={customer.descrpition}
+              name={customer.name}
+              position={customer.position}
+            />
+          ))}
+        </ul>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+      </div>
     </div>
   );
 }
 
 function ContentCustomer({ img, descrpition, name, position }) {
   return (
-    <div className="content">
-      <div className="customer-img">
-        <img src={img} alt={name} />
-        <span className="icon-customer-img">
-          <i class="fa-solid fa-quote-left"></i>
-        </span>
-      </div>
-      <div className="desc-coustomer">
-        <p className="content-desc-coustomer">{descrpition}</p>
-      </div>
-      <div className="customer-info">
-        <h5 className="customer-name">{name}</h5>
-        <span className="customer-position">{position}</span>
+    <div className="slider-list swiper-slide">
+      <div className="content ">
+        <div className="customer-img">
+          <img src={img} alt={name} />
+          <span className="icon-customer-img">
+            <i class="fa-solid fa-quote-left"></i>
+          </span>
+        </div>
+        <div className="desc-coustomer">
+          <p className="content-desc-coustomer">{descrpition}</p>
+        </div>
+        <div className="customer-info">
+          <h5 className="customer-name">{name}</h5>
+          <span className="customer-position">{position}</span>
+        </div>
       </div>
     </div>
   );
